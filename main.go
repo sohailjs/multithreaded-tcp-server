9b8e0244650cfe2e2f7bbef24aadeab1b39f60eb
sohailjs <sohailjs@gmail.com>
2025-01-22 11:54:12 +0530

    more organized and better code

--- main.go
+++ main.go
@@ -6,28 +6,59 @@ import (
 	"net"
 )
 
-func readMessage(conn net.Conn) {
-	buf := make([]byte, 1024)
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Printf("error reading from connection")
+type Server struct {
+	address string
+	ln      net.Listener
+	quitCh  chan struct{}
+}
+
+func NewServer(address string) *Server {
+	return &Server{
+		address: address,
+		quitCh:  make(chan struct{}),
 	}
-	fmt.Printf("received data: %s", string(buf))
-	conn.Write([]byte("hello from server"))
-	//conn.Close()
+}
+func (server *Server) start() error {
+	ln, err := net.Listen("tcp", server.address)
+	if err != nil {
+		return err
+	}
+	server.ln = ln
+
+	//accept connections
+	server.Accept()
+	<-server.quitCh
+	return nil
+}
+
+func (server *Server) Accept() {
+	for {
+		conn, err := server.ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %s", err)
+			continue
+		}
+		fmt.Printf("connected client: %s", conn.RemoteAddr())
+		go server.Read(conn)
+	}
+}
+
+func (server *Server) Read(conn net.Conn) {
+	defer conn.Close()
+	buf := make([]byte, 1024)
+	for {
+		_, err := conn.Read(buf)
+		if err != nil {
+			log.Printf("error reading from connection")
+		}
+		fmt.Printf("\nmessage from client %s: %s", conn.RemoteAddr(), string(buf))
+	}
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":3000")
+	server := NewServer(":3000")
+	err := server.start()
 	if err != nil {
 		log.Fatal(err)
-	}
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Printf("connected client: %s\n", conn.RemoteAddr().String())
-		go readMessage(conn)
 	}
 }
